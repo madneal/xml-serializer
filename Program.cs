46b4dbd62fbd69f9df1dea2d126f04357da35e8c
neal1991 <neal1991@sina.com>
2020-04-20 22:24:11 +0800

    add project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace xml_serializer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
--- TestClass.cs
+++ TestClass.cs
@@ -1,35 +1,40 @@
 using System;
+using System.IO;
+using System.Xml.Serialization;
 
-[XmlRoot]
-public class TestClass {
-	private string classname;
-	private string name;
-	private int age;
-	[XmlAttribute]
-	public string Classname { get => classname; set => classname = value; }
-	[XmlElement]
-	public string Name { get => name; set => name = value; }
-	[XmlElement]
-	public int Age { get => age; set => age = value; }
-	public override string ToString()
-	{
-		return base.ToString();
-	}
-}
+namespace BasicXmlSerializer {
 
-public class MainWindow
-{
-	public MainWindow()
-	{
-		TestClass testClass = new TestClass();
-		testClass.Classname = "test";
-		testClass.Name = "neal";
-		testClass.Age = 18;
-		FileStream fileStream = File.OpenWrite(@"d:\test.txt");
-		using (TextWriter writer = new StreamWriter(fileStream))
+	[XmlRoot]
+	public class TestClass {
+		private string classname;
+		private string name;
+		private int age;
+		[XmlAttribute]
+		public string Classname { get => classname; set => classname = value; }
+		[XmlElement]
+		public string Name { get => name; set => name = value; }
+		[XmlElement]
+		public int Age { get => age; set => age = value; }
+		public override string ToString()
 		{
-			XmlSerializer serializers = new XmlSerializer(typeof(TestClass));
-			serializers.Serialize(writer, testClass);
+			return base.ToString();
+		}
+	}
+
+	public class MainWindow
+	{
+		public MainWindow()
+		{
+			TestClass testClass = new TestClass();
+			testClass.Classname = "test";
+			testClass.Name = "neal";
+			testClass.Age = 18;
+			FileStream fileStream = File.OpenWrite("test.txt");
+			using (TextWriter writer = new StreamWriter(fileStream))
+			{
+				XmlSerializer serializers = new XmlSerializer(typeof(TestClass));
+				serializers.Serialize(writer, testClass);
+			}
 		}
 	}
 }
