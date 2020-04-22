e53aad7957d31be79727ec2a79e65f84aa53f514
neal1991 <neal1991@sina.com>
2020-04-22 16:42:25 +0800

    add json

---
+++ js/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace js
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TestClass testClass = new TestClass();
+            testClass.Name = "mmadneal";
+            testClass.Classname = "neal";
+            testClass.Age = 18;
+            JavaScriptSerializer jss = new JavaScriptSerializer();
+            var jsonReq = jss.Serialize(testClass);
+            Console.WriteLine(jsonReq);
+            Console.ReadKey();
+
+            
+        }
+    }
+
+    public class TestClass
+    {
+        private string classname;
+        private string name;
+        private int age;
+        public string Classname { get => classname; set => classname = value; }
+        public string Name { get => name; set => name = value; }
+        public int Age { get => age; set => age = value; }
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+
+        public static void ClassMethod(string value)
+        {
+            Process.Start(value);
+        }
+    }
+}
